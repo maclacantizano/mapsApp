92f964e094b0e555960312328f236fd787022ea6
maclacantizano <macedocantizano@gmail.com>
2022-08-25 14:35:57 -0300

    6 files modified

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-tab1',
@@ -6,6 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['tab1.page.scss']
 })
 export class Tab1Page {
+  @ViewChild('map') mapRef: ElementRef;
+
+  map: google.maps.Map;
 
   constructor() {}
 
